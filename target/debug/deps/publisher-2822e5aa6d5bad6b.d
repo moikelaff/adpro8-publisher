76a57fbfbbaf36793ba095cdd3f6a195705de77c
moikelaff <dylan.adiprawira@gmail.com>
2025-05-16 19:54:15 +0700

    feat: init repo

---
+++ target/debug/deps/publisher-2822e5aa6d5bad6b.d
@@ -0,0 +1,5 @@
+C:\Users\OMEN\Desktop\codes\adpro_tutorial\tutorial8-publisher\publisher\target\debug\deps\libpublisher-2822e5aa6d5bad6b.rmeta: src\main.rs
+
+C:\Users\OMEN\Desktop\codes\adpro_tutorial\tutorial8-publisher\publisher\target\debug\deps\publisher-2822e5aa6d5bad6b.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/publisher-8a17df1547319b59.d
@@ -0,0 +1,5 @@
+C:\Users\OMEN\Desktop\codes\adpro_tutorial\tutorial8-publisher\publisher\target\debug\deps\libpublisher-8a17df1547319b59.rmeta: src\main.rs
+
+C:\Users\OMEN\Desktop\codes\adpro_tutorial\tutorial8-publisher\publisher\target\debug\deps\publisher-8a17df1547319b59.d: src\main.rs
+
+src\main.rs:
